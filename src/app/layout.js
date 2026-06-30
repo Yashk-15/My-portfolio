01686be74ca76fd27796fa5d1407a5fd188c5a96
Yashk-15 <yashsk1505@gmail.com>
2026-06-30 15:50:01 +0530

    UI changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,13 @@
 import "./globals.css";
-import { Geist, Inter } from "next/font/google";
+import { Sora } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 
-const geist = Geist({
-  subsets: ["latin"],
-  variable: "--font-geist",
-  weight: ["400", "500", "600", "700"],
-});
-
-const inter = Inter({
+const sora = Sora({
   subsets: ["latin"],
   display: "swap",
-  variable: "--font-inter",
+  variable: "--font-sora",
+  weight: ["300", "400", "500", "600", "700", "800"],
   preload: true,
 });
 
@@ -31,7 +26,7 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geist.variable} ${inter.variable}`}>
+    <html lang="en" className={`${sora.variable}`}>
       <body>
         {children}
         <SpeedInsights />
